7a6cf3ee2f001c5ebc5d8126e63c12080c6292d3
rayanfar <younes.rayanfar@gmail.com>
2025-04-23 10:33:28 +0330

    save

--- gregorian_to_jalali.c
+++ gregorian_to_jalali.c
@@ -12,42 +12,40 @@
 
 typedef struct { int year, month, day; } date_struct;
 
-/* Leap-year check for Gregorian calendar */
-static int is_gregorian_leap(int y) {
-    return ( (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0) );
+/* Days in Jalali month */
+static int jalali_month_days(int m) {
+    return (m <= 6) ? 31 : ((m <= 11) ? 30 : 29);
 }
 
-/* Days in each Gregorian month */
-static int days_in_gregorian_month(int y, int m) {
-    static const int md[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-    if (m == 2 && is_gregorian_leap(y))
-        return 29;
+/* Days in Gregorian month */
+static int gregorian_month_days(int y, int m) {
+    static const int md[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    if (m == 2) {
+        if ((y % 400 == 0) || ((y % 4 == 0) && (y % 100 != 0)))
+            return 29;
+    }
     if (m >= 1 && m <= 12)
         return md[m-1];
     return 0;
 }
 
-/* Days in each Jalali month */
-static int days_in_jalali_month(int m) {
-    static const int jd[12] = {31,31,31,31,31,31,30,30,30,30,30,29};
-    if (m >= 1 && m <= 12)
-        return jd[m-1];
-    return 0;
-}
-
 /* Convert Gregorian to Jalali */
-date_struct gregorian_to_jalali_core(int gy, int gm, int gd) {
+static date_struct gregorian_to_jalali_core(int gy, int gm, int gd) {
     date_struct res;
-    int g_day_no = 365 * (gy - 1) + (gy - 1)/4 - (gy - 1)/100 + (gy - 1)/400;
-    for (int i = 1; i < gm; ++i)
-        g_day_no += days_in_gregorian_month(gy, i);
-    g_day_no += gd - 1;
+    int gy2 = gy - 1600;
+    int gm2 = gm - 1;
+    int gd2 = gd - 1;
 
-    int j_day_no = g_day_no - 79;
-    int j_np = j_day_no / 12053;
+    long g_day_no = 365L * gy2 + (gy2 + 3) / 4 - (gy2 + 99) / 100 + (gy2 + 399) / 400;
+    for (int i = 0; i < gm2; ++i)
+        g_day_no += gregorian_month_days(gy, i+1);
+    g_day_no += gd2;
+
+    long j_day_no = g_day_no - 79;
+    long j_np = j_day_no / 12053;
     j_day_no %= 12053;
 
-    int jy = 979 + 33 * j_np + 4 * (j_day_no / 1461);
+    long jy = 979 + 33 * j_np + 4 * (j_day_no / 1461);
     j_day_no %= 1461;
 
     if (j_day_no >= 366) {
@@ -56,47 +54,53 @@ date_struct gregorian_to_jalali_core(int gy, int gm, int gd) {
     }
 
     int jm = 0;
-    int jd_day = 0;
+    int jd = 0;
     for (int i = 1; i <= 12; ++i) {
-        int dim = days_in_jalali_month(i);
+        int dim = jalali_month_days(i);
         if (j_day_no < dim) {
             jm = i;
-            jd_day = j_day_no + 1;
+            jd = j_day_no + 1;
             break;
         }
         j_day_no -= dim;
     }
 
-    res.year = jy;
+    res.year = (int)jy;
     res.month = jm;
-    res.day = jd_day;
+    res.day = jd;
     return res;
 }
 
 /* Convert Jalali to Gregorian */
-date_struct jalali_to_gregorian_core(int jy, int jm, int jd) {
+static date_struct jalali_to_gregorian_core(int jy, int jm, int jd) {
     date_struct res;
-    int j_day_no = (jy - 979) * 365 + ((jy - 979)/33) * 8 + (((jy - 979) % 33 + 3)/4);
-    for (int i = 1; i < jm; ++i)
-        j_day_no += days_in_jalali_month(i);
-    j_day_no += jd - 1;
+    int jy2 = jy - 979;
+    int jm2 = jm - 1;
+    int jd2 = jd - 1;
 
-    int g_day_no = j_day_no + 79;
-    int gy = 1600 + 400 * (g_day_no / 146097);
+    long j_day_no = 365L * jy2 + (jy2 / 33) * 8 + ((jy2 % 33 + 3) / 4);
+    for (int i = 0; i < jm2; ++i)
+        j_day_no += jalali_month_days(i+1);
+    j_day_no += jd2;
+
+    long g_day_no = j_day_no + 79;
+    long gy = 1600 + 400 * (g_day_no / 146097);
     g_day_no %= 146097;
+
     int leap = 1;
     if (g_day_no >= 36525) {
         g_day_no--;
         gy += 100 * (g_day_no / 36524);
         g_day_no %= 36524;
-        if (g_day_no >= 365) {
+        if (g_day_no >= 365)
             g_day_no++;
-        } else {
+        else
             leap = 0;
-        }
     }
+
     gy += 4 * (g_day_no / 1461);
     g_day_no %= 1461;
+
     if (g_day_no >= 366) {
         leap = 0;
         g_day_no--;
@@ -107,7 +111,11 @@ date_struct jalali_to_gregorian_core(int jy, int jm, int jd) {
     int gm = 0;
     int gd = 0;
     for (int i = 1; i <= 12; ++i) {
-        int dim = days_in_gregorian_month(gy, i) + (i == 2 && leap);
+        int dim = gregorian_month_days((int)gy, i);
+        if (i == 2 && !leap)
+            dim = 28;
+        else if (i == 2 && leap)
+            dim = 29;
         if (g_day_no < dim) {
             gm = i;
             gd = g_day_no + 1;
@@ -116,7 +124,7 @@ date_struct jalali_to_gregorian_core(int jy, int jm, int jd) {
         g_day_no -= dim;
     }
 
-    res.year = gy;
+    res.year = (int)gy;
     res.month = gm;
     res.day = gd;
     return res;
@@ -138,7 +146,7 @@ char *gregorian_to_jalali(UDF_INIT *initid, UDF_ARGS *args,
                            char *is_null, char *error) {
     int y, m, d;
     if (sscanf(args->args[0], "%d-%d-%d", &y, &m, &d) != 3 ||
-        y < 1 || y > 9999 || m < 1 || m > 12 || d < 1 || d > days_in_gregorian_month(y, m)) {
+        y < 1 || y > 9999 || m < 1 || m > 12 || d < 1 || d > gregorian_month_days(y, m)) {
         *is_null = 1;
         return NULL;
     }
@@ -167,7 +175,7 @@ char *jalali_to_gregorian(UDF_INIT *initid, UDF_ARGS *args,
                            char *is_null, char *error) {
     int y, m, d;
     if (sscanf(args->args[0], "%d-%d-%d", &y, &m, &d) != 3 ||
-        y < 1 || y > 9999 || m < 1 || m > 12 || d < 1 || d > days_in_jalali_month(m)) {
+        y < 1 || y > 9999 || m < 1 || m > 12 || d < 1 || d > jalali_month_days(m)) {
         *is_null = 1;
         return NULL;
     }
